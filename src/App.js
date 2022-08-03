87e4bb5fc0ac5c76188da1e0efd246de0cba0903
Euri Tanaka <dev@mugendiary.com>
2022-08-03 10:30:30 +0900

    testing commits, updating text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lets Do This!
         </a>
       </header>
     </div>
